f33999b9654ca6444d8c1b1385cd8b4f7f25b00f
Deepak Khicher <deepkhicher321@gmail.com>
2025-09-27 14:53:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (78.24%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,17 +1,27 @@
 class Solution {
                 public:
                     int addDigits(int num) {
-                        while(num>9){
-                            int result =0;
-                            while(num > 0){
+                        // optimal
+                        if(num == 0 ){
+                            return 0;
+                        }
+                        if(num%9==0){
+                            return 9;
+                        }
+                        
+                        return num%9;
+                        // brute force below --
+                        // while(num>9){
+                        //     int result =0;
+                        //     while(num > 0){
                                 
-                                result += num%10;
-                                num /=10;
-                                }
-                            num = result;
-                            }
+                        //         result += num%10;
+                        //         num /=10;
+                        //         }
+                        //     num = result;
+                        //     }
                 
-                        return num;
+                        // return num;
                         
                     }
                 };
